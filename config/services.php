9dc90d50e4fdecbc3a8cd38fb85617edc54fe5c1
Nguyễn Văn Đăng Khoa <khoanguyen15112002@gmail.com>
2025-01-18 00:23:30 +0700

    Update .env.example, services.php, and users_table migration

--- config/services.php
+++ config/services.php
@@ -34,13 +34,13 @@ return [
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' => 'http://127.0.0.1:8000/auth/google/call-back',
+        'redirect' => env('APP_URL').'/auth/google/call-back',
     ],
     'facebook' => [
         'client_id' => env('FACEBOOK_CLIENT_ID'),
         'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
         'redirect' => 'http://127.0.0.1:8000/auth/facebook/callback',
     ],
-    
+
 
 ];
